dd80e1bc89f38bd61f3a9a5a515de11d8471559a
李佳 <jia.li@xinchan.com>
2019-07-12 11:27:16 +0800

    Initial Commit

---
+++ MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by yooli on 2019/7/12.
+//  Copyright © 2019年 lijia-company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MyApp
+//
+//  Created by yooli on 2019/7/12.
+//  Copyright © 2019年 lijia-company. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
